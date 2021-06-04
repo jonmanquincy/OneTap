6e150eaa582c0d6e0a8704beb6819f0d6aba8b5e
jonmanquincy <53950487+jonmanquincy@users.noreply.github.com>
2021-06-04 13:23:12 -0600

    Draws Button
    No functionality just draws a button in the center of the screen successfully.

---
+++ OneTap.Core/Input/CounterButton.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OneTap.Core.Input
+{
+    public class CounterButton
+    {
+
+        private readonly Vector2 baseScreenSize;
+        private Matrix globalTransformation;
+        public readonly Texture2D texture;
+
+        public CounterButton(Texture2D texture)
+        {
+            this.texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            var spriteCenter = new Vector2(64, 64);
+
+            spriteBatch.Draw(texture, new Vector2(64, baseScreenSize.Y - 64), null, Color.White, -MathHelper.PiOver2, spriteCenter, 1, SpriteEffects.None, 0);
+        }
+
+        public GamePadState GetState(TouchCollection touchState, GamePadState gpState)
+        {
+            //Work out what buttons are pressed based on the touchState
+            Buttons buttonsPressed = 0;
+
+            foreach (var touch in touchState)
+            {
+                if (touch.State == TouchLocationState.Moved || touch.State == TouchLocationState.Pressed)
+                {
+                    //Scale the touch position to be in _baseScreenSize coordinates
+                    Vector2 pos = touch.Position;
+                    Vector2.Transform(ref pos, ref globalTransformation, out pos);
+
+                    if (pos.X < 128)
+                        buttonsPressed |= Buttons.DPadLeft;
+                    else if (pos.X < 256)
+                        buttonsPressed |= Buttons.DPadRight;
+                    else if (pos.X >= baseScreenSize.X - 128)
+                        buttonsPressed |= Buttons.A;
+                }
+            }
+
+            //Combine the buttons of the real gamepad
+            var gpButtons = gpState.Buttons;
+            buttonsPressed |= (gpButtons.A == ButtonState.Pressed ? Buttons.A : 0);
+            buttonsPressed |= (gpButtons.B == ButtonState.Pressed ? Buttons.B : 0);
+            buttonsPressed |= (gpButtons.X == ButtonState.Pressed ? Buttons.X : 0);
+            buttonsPressed |= (gpButtons.Y == ButtonState.Pressed ? Buttons.Y : 0);
+
+            buttonsPressed |= (gpButtons.Start == ButtonState.Pressed ? Buttons.Start : 0);
+            buttonsPressed |= (gpButtons.Back == ButtonState.Pressed ? Buttons.Back : 0);
+
+            buttonsPressed |= gpState.IsButtonDown(Buttons.DPadDown) ? Buttons.DPadDown : 0;
+            buttonsPressed |= gpState.IsButtonDown(Buttons.DPadLeft) ? Buttons.DPadLeft : 0;
+            buttonsPressed |= gpState.IsButtonDown(Buttons.DPadRight) ? Buttons.DPadRight : 0;
+            buttonsPressed |= gpState.IsButtonDown(Buttons.DPadUp) ? Buttons.DPadUp : 0;
+
+            buttonsPressed |= (gpButtons.BigButton == ButtonState.Pressed ? Buttons.BigButton : 0);
+            buttonsPressed |= (gpButtons.LeftShoulder == ButtonState.Pressed ? Buttons.LeftShoulder : 0);
+            buttonsPressed |= (gpButtons.RightShoulder == ButtonState.Pressed ? Buttons.RightShoulder : 0);
+
+            buttonsPressed |= (gpButtons.LeftStick == ButtonState.Pressed ? Buttons.LeftStick : 0);
+            buttonsPressed |= (gpButtons.RightStick == ButtonState.Pressed ? Buttons.RightStick : 0);
+
+            var buttons = new GamePadButtons(buttonsPressed);
+
+            return new GamePadState(gpState.ThumbSticks, gpState.Triggers, buttons, gpState.DPad);
+        }
+    }
+}
---
+++ OneTap.Core/TapGame.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using OneTap.Core.Input;
+
+namespace OneTap
+{
+    public class TapGame : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private TouchCollection touchState;
+        private CounterButton button;
+        private GamePadState gamePadState;
+
+        Vector2 baseScreenSize = new Vector2(800, 480);
+        int backbufferWidth, backbufferHeight;
+        private Matrix globalTransformation;
+
+        private Viewport viewport;
+
+        public TapGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = false;
+
+            _graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            button = new CounterButton(Content.Load<Texture2D>("red_button"));
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+
+            // Handle polling for our input and handling high-level input
+            HandleInput();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            Color backgroundColor = new Color(51, 57, 55);
+            GraphicsDevice.Clear(backgroundColor);
+            viewport = _graphics.GraphicsDevice.Viewport;
+            var screenCenter = new Vector2(viewport.Width / 2f, viewport.Height / 2f);
+            Vector2 imageCenter = new Vector2(button.texture.Width / 2f, button.texture.Height / 2f);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(button.texture, screenCenter, null, Color.White, 0f, imageCenter, 8f, SpriteEffects.None, 0f);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void ScalePresentationArea()
+        {
+            //Work out how much we need to scale our graphics to fill the screen
+            backbufferWidth = GraphicsDevice.PresentationParameters.BackBufferWidth;
+            backbufferHeight = GraphicsDevice.PresentationParameters.BackBufferHeight;
+            float horScaling = backbufferWidth / baseScreenSize.X;
+            float verScaling = backbufferHeight / baseScreenSize.Y;
+            Vector3 screenScalingFactor = new Vector3(horScaling, verScaling, 1);
+            globalTransformation = Matrix.CreateScale(screenScalingFactor);
+            System.Diagnostics.Debug.WriteLine("Screen Size - Width[" + GraphicsDevice.PresentationParameters.BackBufferWidth + "] Height [" + GraphicsDevice.PresentationParameters.BackBufferHeight + "]");
+        }
+
+        private void HandleInput()
+        {
+            // get all of our input states
+            touchState = TouchPanel.GetState();
+            //gamePadState = button.GetState(touchState, GamePad.GetState(PlayerIndex.One));
+        }
+    }
+}
